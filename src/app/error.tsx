07f0153fb9744ed04b6106790d17bd0527e3ef77
augustofanck <augustofanck14@gmail.com>
2024-05-14 18:10:57 -0300

    fix: errorComponent update: readme.md

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,21 +2,20 @@
 
 import { useEffect } from "react";
 
-interface errorProps {
+interface ErrorProps {
     error: any;
-    reset: any;
+    reset: () => void;
 }
 
-const error: React.FC<errorProps> = ({error, reset}) => {
+export default function ErrorComponent({ error, reset }: ErrorProps) {
     useEffect(() => {
         console.log(error);
     }, [error]);
+
     return (
         <div className="text-center mt-10">
             <h1>Algo deu errado!</h1>
             <button className="hover:text-amber-600" onClick={() => reset()}>Tente novamente</button>
         </div>
-    )
+    );
 }
-
-export default error
