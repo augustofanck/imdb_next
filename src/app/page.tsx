d217e6a421b4b5a9eacd2a74abba0b44b82deb1f
augustofanck <augustofanck14@gmail.com>
2024-05-14 16:40:06 -0300

    add: loading effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,8 @@ const Home: React.FC<HomeProps> = async ({ searchParams }) => {
   const genre = searchParams.genre || 'fetchTrending';
   const res = await fetch(
     `https://api.themoviedb.org/3${
-      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=pt-BR&page=1`
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=pt-BR&page=1`,
+      { next: { revalidate: 10000 } }
   );
   const data = await res.json();
   if(!res.ok){
