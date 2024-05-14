a1e60f820873e891c5b8a08322ce4b37a9d8895e
augustofanck <augustofanck14@gmail.com>
2024-05-14 16:20:21 -0300

    add: create fetch data TMDB api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+interface HomeProps {
+  searchParams: any;
+}
+
+const Home: React.FC<HomeProps> = async ({ searchParams }) => {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=pt-BR&page=1`
+  );
+  const data = await res.json();
+  if(!res.ok){
+    throw new Error('Falha na busca de dados. Tente novamente.')
+  }
+  const results = data.results;
+
   return (
-    <div>Home</div>
+    <div>
+      <Results results={results}/>
+    </div>
   )
 }
+
+export default Home
---
+++ src/components/Results.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface Result {
+    id: number;
+    original_title: string;
+}
+
+interface ResultsProps {
+    results: Result[];
+}
+
+const Results: React.FC<ResultsProps> = ({ results }) => {
+    return (
+        <div>
+            {
+                results.map((result) => (
+                    <div key={result.id}>
+                        <h2>{result.original_title}</h2>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Results
